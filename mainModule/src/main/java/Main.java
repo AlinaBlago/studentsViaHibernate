e1fcbf74bbc2bee27be1821befa9ecb130fe99e7
Alina <alinablago19@gmail.com>
2020-08-28 11:38:00 +0300

    Modified main

--- mainModule/src/main/java/Main.java
+++ mainModule/src/main/java/Main.java
@@ -16,45 +16,17 @@ public class Main {
           //  GenericDAO group = new GroupDAOImpl(sessionFactory);
           //  group.create(new Group("HTML1"));
 
-//            StudentDAOImpl lessonTest = new StudentDAOImpl(sessionFactory);
-//            lessonTest.nearStudentLessonById(2L);
+            StudentDAOImpl lessonTest = new StudentDAOImpl(sessionFactory);
+            lessonTest.nearStudentLessonById(2L);
 
-            TeacherDAOImpl teacherDAO = new TeacherDAOImpl(sessionFactory);
-            teacherDAO.moreTeacherSuccessfulGroupById(1L);
-
-
-//            Group group = new Group();
-//            group.setValue("Number 3333");
-
-//            Course course = new Course();
-//            course.setValue("Java123");
-//
-//
-//            //CourseDAO courseDAO = new CourseDAOImpl(sessionFactory);
-//            //List<Course> courses = courseDAO.findAll();
-//
-//
-//            //GroupDAO groupDAO = new GroupDAOImpl(sessionFactory);
-//            //List<Group> groups = groupDAO.findAll();
-//
-//
-//            Factory.getInstance().getStudentDAO().addStudent(group);
-//            Factory.getInstance().getStudentDAO().addStudent(course);
-//            Student student1 = new Student("Alina", "Bl", group, course);
-//            Factory.getInstance().getStudentDAO().addStudent(student1);
-
+//            TeacherDAOImpl teacherDAO = new TeacherDAOImpl(sessionFactory);
+//            teacherDAO.moreTeacherSuccessfulGroupById(1L);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
-
-
-
-
-
-
 //                session.getTransaction().commit();
 //            } catch (Exception e) {
 //                session.getTransaction().rollback();
